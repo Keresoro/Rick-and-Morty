32d599584e227b2b3ea4aa98092bf24ff31ed64a
Keresoro <38075826+Keresoro@users.noreply.github.com>
2019-03-27 04:53:51 +0000

    Rick and Morty Map with Rick, Spaceship, Katsarida, Portal

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private Animator _animator;
+
+    public GameObject OpenPanel = null;
+
+    private bool _isInsideTrigger = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetBool("open", false);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = true;
+            OpenPanel.SetActive(true);
+        }  
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _isInsideTrigger = false;
+            _animator.SetBool("open", false);
+            OpenPanel.SetActive(false);
+        }
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isInsideTrigger == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                OpenPanel.SetActive(false);
+                _animator.SetBool("open", true);
+            }
+        }
+    }
+}
